6299cde6ad0831464bca47855f7dbaa88fe26ad4
lmantes <lmantes@bitbucket.org>
2016-10-03 12:11:17 +0800

    fix int from in

--- run.py
+++ run.py
@@ -15,5 +15,5 @@ service.add_routes(app)
 upload.add_routes(app)
 
 if __name__ == '__main__':
-    port = in(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 5000))
     app.run(debug=True, host='0.0.0.0', port=port)

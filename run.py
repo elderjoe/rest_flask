9b55700d71050cc92fc4891817506672ccc68da9
lmantes <lmantes@bitbucket.org>
2016-12-20 16:25:59 +0800

    Added site_config.py for environment variables. Issue #2.

--- run.py
+++ run.py
@@ -3,12 +3,13 @@ import service.routes
 import view_upload.routes
 import staticVariable
 import os
+import site_config as config
 
 staticVariable.check_upload_directory()
 
 app = Flask(__name__)
 
-app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
+app.secret_key = config.SECRET_KEY
 
 service = service.routes
 upload = view_upload.routes
@@ -17,5 +18,5 @@ service.add_routes(app)
 upload.add_routes(app)
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    app.run(debug=True, host='0.0.0.0', port=port)
+    port = int(os.environ.get("PORT", config.PORT))
+    app.run(debug=True, host=config.HOST, port=port)
---
+++ site_config.py
@@ -0,0 +1,4 @@
+SECRET_KEY = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
+DEBUG = False
+PORT = 5000
+HOST = '0.0.0.0'
